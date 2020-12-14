c6100244ec1750388f1a89aa95605932ef51da1d
DiegoASC33 <DiegoStorys@gmail.com>
2020-12-14 12:29:38 -0300

    *Cambios secundarios

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 
     'description': """ En este módulo se gestiona el inventario    """,
 
-    'author': "Diego Sainas",
+    'author': "Diego Salinas",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
@@ -20,12 +20,12 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views_catastro.xml',
+        'security/ir.model.access.csv',
+        'views/view_catastro.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+     #   'demo/demo.xml',
+    #],
 }
--- models/producto.py
+++ models/producto.py
@@ -3,10 +3,10 @@
 from odoo import models, fields, api
 
 class Producto(models.Model):
-    _name = 'catastro.producto'
+        _name = 'catastro.producto'
+        
+        name = fields.Char(String = "Nombre", required=True)
+        precio = fields.Integer(String = "Precio", required=True)
+        stock = fields.Integer(String = "Stock")
 
-    name = fields.Char(String = "Nombre", required=True)
-    precio = fields.Integer(String = "Precio", required=True)
-    stock = fields.Integer(String = "Stock")
-
-
+    
